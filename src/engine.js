d136fd7299e48de40fbee388b83614cd568085b3
AlloVince <allo.vince@gmail.com>
2016-05-24 14:09:31 +0800

    add annotations

--- src/engine.js
+++ src/engine.js
@@ -23,7 +23,18 @@ export {
 };
 
 let app = null;
+
+/**
+ * @class EvaEngine
+ */
 export default class EvaEngine {
+  /**
+   * @param {string} projectRoot
+   * @param {string} configPath
+   * @param {string} sourceRoot
+   * @param {number} port
+   * @param {string} mode
+   */
   constructor({
     projectRoot,
     configPath,
@@ -59,10 +70,16 @@ export default class EvaEngine {
     this.logger.info('Engine started, Meta:', this.meta);
   }
 
+  /**
+   * @returns {{mode: string, port: *, projectRoot, configPath, sourceRoot}|*}
+   */
   getMeta() {
     return this.meta;
   }
 
+  /**
+   * @returns {DI}
+   */
   getDI() {
     return DI;
   }
@@ -78,10 +95,16 @@ export default class EvaEngine {
     return app;
   }
 
+  /**
+   * @returns {express}
+   */
   static createRouter() {
     return express.Router(); //eslint-disable-line new-cap
   }
 
+  /**
+   * @returns {yargs}
+   */
   getCLI() {
     this.registerServiceProviders(EvaEngine.getServiceProvidersForCLI());
     const [, , commandName] = process.argv;
@@ -116,6 +139,9 @@ export default class EvaEngine {
     return argv;
   }
 
+  /**
+   * @returns {[]}
+   */
   static getBaseServiceProviders() {
     return [
       ServiceProviders.EnvProvider,
@@ -124,10 +150,16 @@ export default class EvaEngine {
     ];
   }
 
+  /**
+   * @returns {string}
+   */
   getCommandName() {
     return this.commandName;
   }
 
+  /**
+   * @returns {[]}
+   */
   static getServiceProvidersForWeb() {
     return [
       ServiceProviders.RedisProvider,
@@ -136,6 +168,9 @@ export default class EvaEngine {
     ];
   }
 
+  /**
+   * @returns {[]}
+   */
   static getMiddlewareProviders() {
     return [
       MiddlewareProviders.SessionMiddlewareProvider,
@@ -144,6 +179,9 @@ export default class EvaEngine {
     ];
   }
 
+  /**
+   * @returns {[]}
+   */
   static getServiceProvidersForCLI() {
     return [
       ServiceProviders.HttpClientProvider,
@@ -151,12 +189,18 @@ export default class EvaEngine {
     ];
   }
 
+  /**
+   * @param {Array} providers
+   */
   registerServiceProviders(providers = []) {
     for (const providerClass of providers) {
       this.registerService(providerClass);
     }
   }
 
+  /**
+   * @param {constructor} ProviderClass
+   */
   registerService(ProviderClass) {
     const provider = new ProviderClass(this);
     if (!(provider instanceof ServiceProviders.ServiceProvider)) {
@@ -165,21 +209,38 @@ export default class EvaEngine {
     provider.register();
   }
 
-  registerCommands(commandClasses) {
-    //TODO 支持数组
-    Object.keys(commandClasses).forEach((commandClassName) => {
-      const commandClass = commandClasses[commandClassName];
-      this.commands[commandClass.getName()] = commandClass;
-    });
+  /**
+   * @param {(Array|Object)}commands
+   * @returns {EvaEngine}
+   */
+  registerCommands(commands) {
+    const registerCommandClass = (commandClasses) => {
+      Object.keys(commandClasses).forEach((commandClassName) => {
+        const commandClass = commandClasses[commandClassName];
+        this.commands[commandClass.getName()] = commandClass;
+      });
+    };
+    if (Array.isArray(commands)) {
+      commands.forEach(registerCommandClass);
+    } else {
+      registerCommandClass(commands);
+    }
     this.logger.debug('Registered commands', Object.keys(this.commands));
     return this;
   }
 
+  /**
+   * @param {Function} handler
+   * @returns {EvaEngine}
+   */
   setDefaultErrorHandler(handler) {
     this.defaultErrorHandler = handler;
     return this;
   }
 
+  /**
+   * @returns {Function}
+   */
   getDefaultErrorHandler() {
     const env = DI.get('env');
     const stackHandler = (stack) => {
@@ -224,11 +285,18 @@ export default class EvaEngine {
       });
   }
 
+  /**
+   * @param {Function} handler
+   * @returns {EvaEngine}
+   */
   setUncaughtExceptionHandler(handler) {
     this.uncaughtExceptionHandler = handler;
     return this;
   }
 
+  /**
+   * @returns {Function}
+   */
   getUncaughtExceptionHandler() {
     return this.uncaughtExceptionHandler ||
       ((err) => {
@@ -245,11 +313,18 @@ export default class EvaEngine {
       });
   }
 
+  /**
+   * @param {Function} handler
+   * @returns {EvaEngine}
+   */
   setServerErrorHandler(handler) {
     this.serverErrorHandler = handler;
     return this;
   }
 
+  /**
+   * @returns {Function}
+   */
   getServerErrorHandler() {
     return this.serverErrorHandler ||
       ((error) => {
@@ -280,6 +355,9 @@ export default class EvaEngine {
       });
   }
 
+  /**
+   * @returns {EvaEngine}
+   */
   bootstrap() {
     this.registerServiceProviders(EvaEngine.getServiceProvidersForWeb());
     this.registerServiceProviders(EvaEngine.getMiddlewareProviders());
@@ -292,6 +370,9 @@ export default class EvaEngine {
     return EvaEngine.getApp().use(...args);
   }
 
+  /**
+   * @returns {EvaEngine}
+   */
   run() {
     process.on('uncaughtException', this.getUncaughtExceptionHandler());
     EvaEngine.getApp().set('port', this.port);
@@ -300,6 +381,7 @@ export default class EvaEngine {
     this.server.listen(this.port);
     this.server.on('error', this.getServerErrorHandler());
     this.logger.info('Engine running http server by listening', this.port);
+    return this;
   }
 
   async runCLI() {
