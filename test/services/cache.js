e91010c29e2a099fd86c4d2522e3d8767f2b888c
theharveyz <geekwebme@gmail.com>
2017-09-04 18:12:12 +0800

    fix ci

--- test/services/cache.js
+++ test/services/cache.js
@@ -30,15 +30,15 @@ test('Cache namespace flush', async(t) => {
 
 test('Cache namespace set nx || xx', async(t) => {
   let ret = await cache.namespace('ns').set('foo', 'bar', 0, 'xx');
-  t.false(ret);
+  t.false(ret !== 'OK');
 
   ret = await cache.namespace('ns').set('foo', 'bar', 0, 'nx');
-  t.true(ret);
+  t.true(ret === 'OK');
   ret = await cache.namespace('ns').set('foo', 'bar', 0, 'nx');
-  t.false(ret);
+  t.false(ret !== 'OK');
   
   ret = await cache.namespace('ns').set('foo', 'bar', 0, 'xx');
-  t.true(ret);
+  t.true(ret === 'OK');
 
   await cache.namespace('ns').flush();
 });
